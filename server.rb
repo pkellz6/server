e6b3020a2cff1e318490839f5d9e7514c9209bfd
Pat Kelly <pkellz6@me.com>
2015-07-15 11:59:46 -0400

    remove all routes

--- server.rb
+++ server.rb
@@ -1,21 +1,25 @@
 require "sinatra"
 
-def hello
-  "Hello World"
-end
 
-get "/" do
- "Hello world"
-end
 
-get "/sinatra" do
-  "Hello Sinatra"
-end
 
-get "/blah" do
-    hello
-end
 
-get "/sinatra" do
-  "Hello Sinatra"
-end
+# def hello
+#   "Hello World"
+# end
+#
+# get "/" do
+#  "Hello world"
+# end
+#
+# get "/sinatra" do
+#   "Hello Sinatra"
+# end
+#
+# get "/blah" do
+#     hello
+# end
+#
+# get "/sinatra" do
+#   "Hello Sinatra"
+# end
